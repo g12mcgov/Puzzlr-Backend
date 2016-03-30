2ef3a4968bad2211b95e76d09d20b9b2a774f957
Grant McGovern <grant@cobrain.com>
2016-03-30 13:19:30 -0400

    Removing nodemon for heroku

--- app.js
+++ app.js
@@ -66,9 +66,9 @@ app.use(function(err, req, res, next) {
 
 
 // Nodemon 
-app.set('port', process.env.PORT || 3000);
-var server = app.listen(app.get('port'), function() {
-	console.log('Express server listening on port ' + server.address().port);
-});
+// app.set('port', process.env.PORT || 3000);
+// var server = app.listen(app.get('port'), function() {
+// 	console.log('Express server listening on port ' + server.address().port);
+// });
 
 module.exports = app;
